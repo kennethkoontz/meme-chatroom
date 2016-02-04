b9496e33ae48d10acfb44738d1b3e87d6b15b4ec
Gus <geekwise.gustavo.jimenez@geekwise.org>
2016-02-04 21:30:55 +0000

    added repeat for profile

--- static/js/profile.controller.js
+++ static/js/profile.controller.js
@@ -1,5 +1,7 @@
 angular
    .module('app')
    .controller('profileController', function($scope) {
-    
+    $scope.post=[
+       {image: "http://stech2.firstpost.com/tech2images/640x359/proportional/jpeg/gallery/2013/aug/facebookprofile_301007399129.jpg" , name:person, picture_name: picture, tag:stuff }
+       ];
 })

32aa5e4a4e8f690a5f5393292f561b0c944822e1
Gus <geekwise.gustavo.jimenez@geekwise.org>
2016-02-04 21:17:04 +0000

    added repeat

--- static/js/feed.controller.js
+++ static/js/feed.controller.js
@@ -1,5 +1,8 @@
 angular
    .module('app')
    .controller('feedController', function($scope) {
-    
+    $scope.post=[
+       {image: "http://stech2.firstpost.com/tech2images/640x359/proportional/jpeg/gallery/2013/aug/facebookprofile_301007399129.jpg" , name:person, picture_name: picture, tag:stuff }
+       ];
+       
 })

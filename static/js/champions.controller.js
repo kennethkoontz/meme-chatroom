6f783612ad4fe85ee6c05ef237e0f2619ac183ea
Gus <geekwise.gustavo.jimenez@geekwise.org>
2016-02-04 21:36:27 +0000

    replaced champions

---
+++ static/js/champions.controller.js
@@ -0,0 +1,7 @@
+angular
+   .module('app')
+   .controller('championsController', function($scope) {
+    $scope.post=[
+       {image: "http://stech2.firstpost.com/tech2images/640x359/proportional/jpeg/gallery/2013/aug/facebookprofile_301007399129.jpg" , name:person, picture_name: picture, tag:stuff }
+       ];
+})

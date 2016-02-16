62af3de2f393a8afed8663c7d851f2e4df6c7499
Montgomery Lara <geekwise.montgomery.lara@gmail.com>
2016-02-16 17:42:41 +0000

    changed flag to Hate

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@
 angular
     .module('app', ['ui.router'])
     .config(function($stateProvider, $urlRouterProvider) {
-      $urlRouterProvider.otherwise("/feed");
+      $urlRouterProvider.otherwise("/signup");
       $stateProvider
         .state('li', {
           abstract: true,

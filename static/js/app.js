17e5724bd2874b78ff908aa0e0f18a76bc22d795
Gus <geekwise.gustavo.jimenez@geekwise.org>
2016-02-16 21:19:04 +0000

    added crtl

--- static/js/app.js
+++ static/js/app.js
@@ -7,60 +7,60 @@ angular
       $stateProvider
         .state('li', {
           abstract: true,
-          templateUrl: "loggedin.html"
+          templateUrl: "loggedin.html as loggedinCtrl"
         })
       
         .state('li.feed', {
           url: "/feed",
           templateUrl: "feed.html",
-          controller: 'FeedController'
+          controller: 'FeedController as FeedCtrl'
         })
         
         .state('frontPg', {
           url: "/frontPg",
           templateUrl: "frontPg.html",
-          controller: 'frontPgcontroller'
+          controller: 'frontPgcontroller as frontPgCtrl'
         })
         
         .state('frontPg_2', {
           url: "/frontPg_2",
           templateUrl: "frontPg_2.html",
-          controller: 'frontPg_2controller'
+          controller: 'frontPg_2controller as frontPg_2Ctrl'
         })
         
         .state('login', {
           url: "/login",
           templateUrl: "login.html",
-          controller: 'logincontroller'
+          controller: 'logincontroller as loginCtrl'
         }) 
         
         .state('li.profile', {
           url: "/profile",
           templateUrl: "profile.html",
-          controller: 'profilecontroller'
+          controller: 'profilecontroller as profileCtrl'
         })  
         
         .state('li.settings', {
           url: "/settings",
           templateUrl: "settings.html",
-          controller: 'settingscontroller'
+          controller: 'settingscontroller as settingsCtrl'
         })  
         
         .state('signup', {
           url: "/signup",
           templateUrl: "signup.html",
-          controller: 'signupcontroller'
+          controller: 'signupcontroller as signupCtrl'
         })
         
         .state('signin', {
           url: "/signin",
           templateUrl: "signin.html",
-          controller: 'signincontroller'
+          controller: 'signincontroller as signinCtrl'
         }) 
         
         .state('li.subs', {
           url: "/subs",
           templateUrl: "subs.html",
-          controller: 'subscontroller'
+          controller: 'subscontroller as subsCtrl'
         })
     });

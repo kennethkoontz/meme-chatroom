f5ba13a45eb76cc9a7f2e3c2e9a1eaa705cd01cf
Montgomery Lara <geekwise.montgomery.lara@gmail.com>
2016-04-04 20:44:15 +0000

    fixed some glitches

--- static/js/feed.controller.js
+++ static/js/feed.controller.js
@@ -3,10 +3,8 @@ angular
    .controller('feedController', function() {
         var vm = this
         vm.post=[
-<<<<<<< HEAD
-           {image: "/images/hotdog.jpg" , name: "Person", picture_name: "picture", tag: "Tag", love:"/images/Champ.png" }
-=======
-           {image: "/images/Anon.jpg" , name: "Person", picture_name: "picture", tag: "Tag" }
->>>>>>> 282d185eccb50efa06397dd5fbf8f9b2d323ddb9
+
+           {image: "/images/hotdog.jpg" , name: "Person", picture_name: "picture", tag: "Tag"}
+
         ];
     });

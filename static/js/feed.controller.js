046d0b2cc73aef4f64d1b5188c0c124b061e44c7
Montgomery Lara <geekwise.montgomery.lara@gmail.com>
2016-04-04 20:34:59 +0000

    changed champ image

--- static/js/feed.controller.js
+++ static/js/feed.controller.js
@@ -3,6 +3,6 @@ angular
    .controller('FeedController', function() {
         var vm = this
         vm.post=[
-           {image: "/images/hotdog.jpg" , name: "Person", picture_name: "picture", tag: "Tag" }
+           {image: "/images/hotdog.jpg" , name: "Person", picture_name: "picture", tag: "Tag", love:"/images/Champ.png" }
         ];
     });
